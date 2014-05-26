40cb441756b2ec625e2b9a1adcb7dadb7674f3da
Liu Yang <liu_yang@worksap.co.jp>
2014-05-26 09:54:09 +0800

    Add test and format code.

--- src/test/java/liuyang/druid/DruidTest.java
+++ src/test/java/liuyang/druid/DruidTest.java
@@ -13,20 +13,38 @@ public class DruidTest {
 
     @Test
     public void druidTest() throws IOException {
-        Map<String, Integer> result = runner.run(" var a;" + " var b;"
-                + " var c;" + " var d;" + " a = 1;" + " b = a;" + " c <- a;"
+        Map<String, Integer> result = runner.run(" var a;"
+                + " var b;"
+                + " var c;"
+                + " var d;"
+                + " a = 1;"
+                + " b = a;"
+                + " c <- a;"
                 + " a = 2;");
         System.out.println(result);
         assertEquals(new Integer(2), result.get("a"));
         assertEquals(new Integer(1), result.get("b"));
         assertEquals(new Integer(2), result.get("c"));
         assertEquals(null, result.get("d"));
+
+        result = runner.run("var a, b ,c;"
+                + " a = 1;"
+                + " b = 2;"
+                + " c <- a + b;"
+                + " a = 3;"
+                + " b = 4;");
+        System.out.println(result);
+        assertEquals(new Integer(3), result.get("a"));
+        assertEquals(new Integer(4), result.get("b"));
+        assertEquals(new Integer(7), result.get("c"));
     }
 
     @Test
     public void testNotInitialized() throws IOException {
         try {
-            runner.run("var a, b; " + " a = b;" + " b = 1;");
+            runner.run("var a, b; "
+                    + " a = b;"
+                    + " b = 1;");
         } catch (IllegalStateException e) {
             assertEquals("variable b is not initialized!", e.getMessage());
         }
@@ -34,8 +52,12 @@ public class DruidTest {
 
     @Test
     public void testMultiDefinition() throws IOException {
-        Map<String, Integer> result = runner.run(" var a, b;" + " a = 1;"
-                + " b = a;" + " var c, d;" + " c <- a;" + " a = 2;");
+        Map<String, Integer> result = runner.run(" var a, b;"
+                + " a = 1;"
+                + " b = a;"
+                + " var c, d;"
+                + " c <- a;"
+                + " a = 2;");
         System.out.println(result);
         assertEquals(new Integer(2), result.get("a"));
         assertEquals(new Integer(1), result.get("b"));
@@ -43,7 +65,10 @@ public class DruidTest {
         assertEquals(null, result.get("d"));
 
         try {
-            runner.run(" var a;" + " b = 1;" + " var b;" + " a = 2;");
+            runner.run(" var a;"
+                    + " b = 1;"
+                    + " var b;"
+                    + " a = 2;");
         } catch (IllegalStateException e) {
             assertEquals("variable b not defined!", e.getMessage());
             ;
@@ -62,7 +87,9 @@ public class DruidTest {
     @Test
     public void testExpr() throws IOException {
         Map<String, Integer> result = runner.run(" var a, b, c, d;"
-                + " a = 1 + 2 - 3;" + " b = 12 / 3 * 2;" + " c = 12 / (3 * 2);"
+                + " a = 1 + 2 - 3;"
+                + " b = 12 / 3 * 2;"
+                + " c = 12 / (3 * 2);"
                 + " d = a - (b + c);");
         System.out.println(result);
         assertEquals(new Integer(0), result.get("a"));
@@ -74,9 +101,11 @@ public class DruidTest {
     @Test
     public void testExprExtend() throws IOException {
         Map<String, Integer> result = runner.run(" var a, b, c, d;"
-                + " a = 1;" + " b = 2;"
-                + " c <-  a + b;" + " d <- c * 2 - (a * b);"
-                        + " a = 3; b <- a + 1; a = 2;");
+                + " a = 1;"
+                + " b = 2;"
+                + " c <-  a + b;"
+                + " d <- c * 2 - (a * b);"
+                + " a = 3; b <- a + 1; a = 2;");
         System.out.println(result);
         assertEquals(new Integer(2), result.get("a"));
         assertEquals(new Integer(3), result.get("b"));
